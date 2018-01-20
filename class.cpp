74abd5d402dee2cf84b64b45010ec7e10050f0aa
shintiasari <32458115+shintiasari@users.noreply.github.com>
2018-01-20 17:16:53 +0800

    Membuat void void dan Selesai.

--- class.cpp
+++ class.cpp
@@ -1,4 +1,4 @@
-#include <iostrem>
+#include <iostream>
 #include <string>
 using namespace std;
 
@@ -8,7 +8,7 @@ class bunga {
 		int bentuk(string bentuk);
 		string wangi;
 		void berasaldari(string asal);
-		void tumbuhdi(int wilayah);
+		void tumbuhdi(string wilayah);
 };
 		
 int main(){
@@ -16,17 +16,17 @@ int main(){
 	bungaester.wangi ="harum sekali";
 	bungasepatu.wangi ="tidak berbau";
 	bungaasoka.wangi = "harum pada malam hari";
-	bungamawar.wangi = "harum sekali bisa dibuat farpum"
-	bungaalamanda.wangi = "harum aja"
-	bungasakura.wangi = "harum aja"
-	bungarafflesia.wangi = "berbau bangkai"
-	bungaelata.wangi = "bau tak sedap"
+	bungamawar.wangi = "harum sekali bisa dibuat farpum";
+	bungaalamanda.wangi = "harum aja";
+	bungasakura.wangi = "harum aja";
+	bungarafflesia.wangi = "berbau bangkai";
+	bungaelata.wangi = "bau tak sedap";
 	
 	cout <<"Ciri-ciri bunga ester: \n\n";
 	cout <<(bungaester.wangi);
 	bungaester.warna("merah,kuning,pink,ungu,biru");
 	bungaester.bentuk("menyerupai bintang");
-	bungaester.berasaldari"Tiongkok");
+	bungaester.berasaldari("Tiongkok");
 	bungaester.tumbuhdi("Indonesia,Cina,Jepang");
 	cout <<"_______________________________\n\n";
 	
@@ -34,7 +34,7 @@ int main(){
 	cout <<(bungasepatu.wangi);
 	bungasepatu.warna("merah,kuning,putih,oranye");
 	bungasepatu.bentuk("seperti terompet");
-	bungasepatu.berasaldari"Indonesia");
+	bungasepatu.berasaldari("Indonesia");
 	bungasepatu.tumbuhdi("Indonesia,Malaysia,Tiongkok,Jepang");
 	cout <<"_______________________________\n\n";
 	
@@ -42,7 +42,7 @@ int main(){
 	cout <<(bungaasoka.wangi);
 	bungaasoka.warna("merah,kuning,jingga");
 	bungaasoka.bentuk("bergerombol dan berderet");
-	bungaasoka.berasaldari"India");
+	bungaasoka.berasaldari("India");
 	bungaasoka.tumbuhdi("Indonesia,Daerah Asia");
 	cout <<"_______________________________\n\n";
 	
@@ -50,7 +50,7 @@ int main(){
 	cout <<(bungamawar.wangi);
 	bungamawar.warna("merah,merah muda,putih,kuning");
 	bungamawar.bentuk("daun mahkota");
-	bungamawar.berasaldari"Cina");
+	bungamawar.berasaldari("Cina");
 	bungamawar.tumbuhdi("Indonesia,Eropa,Tiongkok,Timur Tengah,Jepang,Amerika Serikat");
 	cout <<"_______________________________\n\n";
 	
@@ -58,7 +58,7 @@ int main(){
 	cout <<(bungaalamanda.wangi);
 	bungaalamanda.warna("kuning");
 	bungaalamanda.bentuk("menyerupai lonceng");
-	bungaalamanda.berasaldari"Amerika Tengah");
+	bungaalamanda.berasaldari("Amerika Tengah");
 	bungaalamanda.tumbuhdi("Indonesia,Brazil,");
 	cout <<"_______________________________\n\n";
 	
@@ -66,7 +66,7 @@ int main(){
 	cout <<(bungasakura.wangi);
 	bungasakura.warna("merah jambu,putih,kuning muda,hijau muda");
 	bungasakura.bentuk("sususan mahkota berlapis");
-	bungasakura.berasaldari"Jepang");
+	bungasakura.berasaldari("Jepang");
 	bungasakura.tumbuhdi("Tokyo,Kyoto,Honshu,Osaka");
 	cout <<"_______________________________\n\n";
 	
@@ -74,7 +74,7 @@ int main(){
 	cout <<(bungarafflesia.wangi);
 	bungarafflesia.warna("merah");
 	bungarafflesia.bentuk("bermahkota lima");
-	bungarafflesia.berasaldari"Indonesia");
+	bungarafflesia.berasaldari("Indonesia");
 	bungarafflesia.tumbuhdi("Indonesia,Filipina,Malaysia,Thailand");
 	cout <<"_______________________________\n\n";
 	
@@ -82,8 +82,20 @@ int main(){
 	cout <<(bungaelata.wangi);
 	bungaelata.warna("merah,oranye");
 	bungaelata.bentuk("menyerupai bibir");
-	bungaelata.berasaldari"Amerika Selatan");
+	bungaelata.berasaldari("Amerika Selatan");
 	bungaelata.tumbuhdi("Kolombia,Panama,Kosta Rika,Ekuador");
-	cout <<"_______________________________\n\n";	
-	
+	cout <<"_______________________________\n\n";		
+}
+
+void bunga::warna(string warna){
+	cout <<"\nwarnanya: "<<warna<<endl;
+}
+int bunga::bentuk(string bentuk){
+	cout <<"bentuknya: "<<bentuk<<endl;
+}
+void bunga::berasaldari(string asal){
+	cout <<"berasal dari: "<<asal<<endl;
+}
+void bunga::tumbuhdi(string wilayah){
+	cout <<"tumbuh di wilayah: "<<wilayah<<endl;
 }
