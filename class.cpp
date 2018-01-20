238ecaecca5d96523f3c93275d4a4614c34cca01
shintiasari <32458115+shintiasari@users.noreply.github.com>
2018-01-20 15:31:29 +0800

    Membuat class bunga

---
+++ class.cpp
@@ -0,0 +1,11 @@
+#include <iostrem>
+#include <string>
+using namespace std;
+
+class bunga {
+	public:
+		void warna(string warna);
+		int wangi(string harum);
+		string bentuk;
+		void berasaldari(string asal);
+		void Tumbuhdi(int wilayah);
