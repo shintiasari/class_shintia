c2e16dab76e14706e97c6a3bb08017827c2b1429
shintiasari <32458115+shintiasari@users.noreply.github.com>
2018-01-20 16:07:37 +0800

    Menambahkan int main untuk class bunga

--- class.cpp
+++ class.cpp
@@ -8,8 +8,17 @@ class bunga {
 		int bentuk(string bentuk);
 		string wangi;
 		void berasaldari(string asal);
-		void Tumbuhdi(int wilayah);\
+		void Tumbuhdi(int wilayah);
+};
 		
 int main(){
-	bunga 
+	bunga bungaester,bungasepatu,bungaasoka,bungamawar,bungaalamanda,bungasakura,bungarafflesia,bungaelata;
+	bungaester.wangi ="harum sekali";
+	bungasepatu.wangi ="tidak berbau";
+	bungaasoka.wangi = "harum pada malam hari";
+	bungamawar.wangi = "harum sekali bisa dibuat farpum"
+	bungaalamanda.wangi = "harum aja"
+	bungasakura.wangi = "harum aja"
+	bungarafflesia.wangi = "berbau bangkai"
+	bungaelata.wangi = "bau tak sedap"
 }
