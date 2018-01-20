25ee820ea9cbce74bbc09fcc0e0cc3d0d090d649
shintiasari <32458115+shintiasari@users.noreply.github.com>
2018-01-20 15:48:17 +0800

    Mengubah string

---
+++ class.cpp
@@ -0,0 +1,15 @@
+#include <iostrem>
+#include <string>
+using namespace std;
+
+class bunga {
+	public:
+		void warna(string warna);
+		int bentuk(string bentuk);
+		string wangi;
+		void berasaldari(string asal);
+		void Tumbuhdi(int wilayah);\
+		
+int main(){
+	bunga 
+}
